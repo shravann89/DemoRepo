6cee74811e7f115daa91b40c74ebc465a7e93b7f
lnc <lnc@DIN76000005.corp.capgemini.com>
2017-06-14 19:26:42 +0530

    syso changes

--- BankApp/src/main/java/org/cap/service/AccountServiceImpl.java
+++ BankApp/src/main/java/org/cap/service/AccountServiceImpl.java
@@ -87,6 +87,7 @@ public class AccountServiceImpl implements AcccountService{
 		int sum=0;
 		for(int i=0;i<arr.length;i++)
 			sum+=arr[i];
+		System.out.println("Helooo git");
 		return sum;
 
 	}
